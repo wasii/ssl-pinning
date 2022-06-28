a7c58f164eda076a88d256597a7254a4b523d89e
abdWasiq <abdul.wasiq@naheed.pk>
2022-06-28 17:21:08 +0500

    project initiliased

---
+++ SSL Pinning/SSL Pinning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SSL Pinning
+//
+//  Created by NaheedPK on 28/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
